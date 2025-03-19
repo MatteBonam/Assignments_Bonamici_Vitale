dc69b8b74d52a7559d3d82d44663c8e128d197df
unknown <matteo.bonamici2512@gmail.com>
2025-03-19 14:40:43 +0100

    Uploaded the first files

---
+++ AlgebraicIdentity/AlgebraicIdentity.cpp
@@ -0,0 +1,162 @@
+//=============================================================================
+// FILE:
+//    TestPass.cpp
+//
+// DESCRIPTION:
+//    Visits all functions in a module and prints their names. Strictly speaking, 
+//    this is an analysis pass (i.e. //    the functions are not modified). However, 
+//    in order to keep things simple there's no 'print' method here (every analysis 
+//    pass should implement it).
+//
+// USAGE:
+//    New PM
+//      opt -load-pass-plugin=<path-to>libTestPass.so -passes="test-pass" `\`
+//        -disable-output <input-llvm-file>
+//
+//
+// License: MIT
+//=============================================================================
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+//-----------------------------------------------------------------------------
+// TestPass implementation
+//-----------------------------------------------------------------------------
+// No need to expose the internals of the pass to the outside world - keep
+// everything in an anonymous namespace.
+namespace {
+
+bool runOnInstruction(Instruction &I) {
+    outs() << "SONO: " << I.getOpcodeName() << "\n";
+    // Stampa la prima istruzione come operando
+    outs() << "COME OPERANDO: ";
+    I.printAsOperand(outs(), false);
+    outs() << "\n";
+
+    // User-->Use-->Value
+    outs() << "I MIEI OPERANDI SONO:\n";
+    for (auto *Iter = I.op_begin(); Iter != I.op_end(); ++Iter) {
+      Value *Operand = *Iter;
+
+      if (Argument *Arg = dyn_cast<Argument>(Operand)) {
+        outs() << "\t" << *Arg << ": SONO L'ARGOMENTO N. " << Arg->getArgNo() 
+	       <<" DELLA FUNZIONE " << Arg->getParent()->getName()
+               << "\n";
+      }
+      else if (ConstantInt *C = dyn_cast<ConstantInt>(Operand)) {
+        outs() << "\t" << *C << ": SONO UNA COSTANTE INTERA DI VALORE " << C->getValue()
+               << "\n";
+      }
+      else {
+        outs() << "\t" << *Operand << ": SONO UN OPERANDO GENERICO"
+               << "\n";
+      }
+    }
+
+    outs() << "LA LISTA DEI MIEI USERS:\n";
+    for (auto Iter = I.user_begin(); Iter != I.user_end(); ++Iter) {
+      outs() << "\t" << *(dyn_cast<Instruction>(*Iter)) << "\n";
+    }
+
+    outs() << "E DEI MIEI USI (CHE E' LA STESSA):\n";
+    for (auto Iter = I.use_begin(); Iter != I.use_end(); ++Iter) {
+      outs() << "\t" << *(dyn_cast<Instruction>(Iter->getUser())) << "\n";
+    }
+
+    if (I.getOpcode() == Instruction::Mul)
+    {
+      outs() << "SONO UNA " << I.getOpcodeName() << " ED HO COME PARAMETRI " << I.getOperand(0) << " E " << I.getOperand(1) << "\n";
+      if (ConstantInt *Con = dyn_cast<ConstantInt>(I.getOperand(1)))
+      {
+        outs() << "SONO IL SECONDO PARAMETRO E SONO UNA COSTANTE DI VALORE: " << *Con << "\n";
+        if (*Con){
+          Value* newValue = new Value();
+        Instruction *NewInst = BinaryOperator::Create(
+          Instruction::Shl, I.getOperand(0), newValue);
+        }
+      }
+      else
+      {
+        outs() << "";
+      }
+    }
+    //   Manipolazione delle istruzioni
+    //Instruction *NewInst = BinaryOperator::Create(
+    //    Instruction::Add, Inst1st.getOperand(0), Inst1st.getOperand(0));
+
+    //NewInst->insertAfter(&Inst1st);
+    // Si possono aggiornare le singole references separatamente?
+    // Controlla la documentazione e prova a rispondere.
+    //Inst1st.replaceAllUsesWith(NewInst);
+    return true;
+}
+
+bool runOnBasicBlock(BasicBlock &B) {
+    
+    // Preleviamo le prime due istruzioni del BB
+    for (auto Iter = B.begin(); Iter != B.end(); ++Iter) {
+    if (runOnInstruction(*Iter)) {
+      }
+    }
+    // L'indirizzo della prima istruzione deve essere uguale a quello del 
+    // primo operando della seconda istruzione (per costruzione dell'esempio)
+
+    
+    return true;
+  }
+
+
+
+// New PM implementation
+struct TestPass: PassInfoMixin<TestPass> {
+  // Main entry point, takes IR unit to run the pass on (&F) and the
+  // corresponding pass manager (to be queried if need be)
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
+
+    bool Transformed = false;
+  	for (auto Iter = F.begin(); Iter != F.end(); ++Iter) {
+    if (runOnBasicBlock(*Iter)) {
+      Transformed = true;
+      }
+    }
+    errs() << Transformed << "\n";
+  	return PreservedAnalyses::all();
+}
+
+
+  // Without isRequired returning true, this pass will be skipped for functions
+  // decorated with the optnone LLVM attribute. Note that clang -O0 decorates
+  // all functions with optnone.
+  static bool isRequired() { return true; }
+};
+} // namespace
+
+//-----------------------------------------------------------------------------
+// New PM Registration
+//-----------------------------------------------------------------------------
+llvm::PassPluginLibraryInfo getTestPassPluginInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "TestPass", LLVM_VERSION_STRING,
+          [](PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](StringRef Name, FunctionPassManager &FPM,
+                   ArrayRef<PassBuilder::PipelineElement>) {
+                  if (Name == "my-test-pass") {
+                    FPM.addPass(TestPass());
+                    return true;
+                  }
+                  return false;
+                });
+          }};
+}
+
+// This is the core interface for pass plugins. It guarantees that 'opt' will
+// be able to recognize TestPass when added to the pass pipeline on the
+// command line, i.e. via '-passes=test-pass'
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return getTestPassPluginInfo();
+}
