0987a545fcce35f00283b84fe4e7645955f9a36b
Leonardo Vitale <leonardo.vitale@fermi.mo.it>
2025-03-30 19:50:50 +0200

    added algebraic edge cases

--- Assignment_1_Bonamici-Vitale/AlgebraicIdentity/AlgebraicIdentity.cpp
+++ Assignment_1_Bonamici-Vitale/AlgebraicIdentity/AlgebraicIdentity.cpp
@@ -4,13 +4,13 @@
 //
 // DESCRIPTION:
 //    Visits all functions in a module and prints their names. Strictly speaking, 
-//    this is an analysis pass (i.e. //    the functions are not modified). However, 
+//    this is an analysis pass (i.e. the functions are not modified). However, 
 //    in order to keep things simple there's no 'print' method here (every analysis 
 //    pass should implement it).
 //
 // USAGE:
 //    New PM
-//      opt -load-pass-plugin=<path-to>libTestPass.so -passes="test-pass" `\`
+//      opt -load-pass-plugin=<path-to>libTestPass.so -passes="test-pass" \
 //        -disable-output <input-llvm-file>
 //
 //
@@ -31,39 +31,70 @@ using namespace llvm;
 namespace {
 
 bool runOnInstruction(Instruction &I) {
+    // opt DIV per 1
+    if ((I.getOpcode() == Instruction::UDiv) || (I.getOpcode() == Instruction::SDiv)) {
+        outs() << "RILEVATA DIV " << "\n";
+        Value *Operand1 = I.getOperand(0);
+        Value *Operand2 = I.getOperand(1);
 
-    if (I.getOpcode() == Instruction::Mul){
-      outs() << "RILEVATA MUL " << "\n";
-      Value *Operand1 = I.getOperand(0);
-      Value *Operand2 = I.getOperand(1);
-      if (ConstantInt *C = dyn_cast<ConstantInt>(Operand1)){
-        outs() << "Opearand 1 -> " << C->getValue() << "\n";
-        if(C->getValue() == 1){
-          I.replaceAllUsesWith(Operand2);
+        if (ConstantInt *D = dyn_cast<ConstantInt>(Operand2)) {
+            if (D->getValue() == 1) {
+                outs() << "SOST DIV / 1 con -> " << D->getValue() << "\n";
+                I.replaceAllUsesWith(Operand1);
+            }
         }
-      }
-      if(ConstantInt *C1 = dyn_cast<ConstantInt>(Operand2)){
-        outs() << "Opearand 2 -> " << C1->getValue() << "\n";
-          if(C1->getValue() == 1){
-            I.replaceAllUsesWith(Operand1);
+    }
+    // opt SUB 0
+    else if (I.getOpcode() == Instruction::Sub) {
+        outs() << "RILEVATA SUB " << "\n";
+        Value *Operand1 = I.getOperand(0);
+        Value *Operand2 = I.getOperand(1);
+
+        if (ConstantInt *D = dyn_cast<ConstantInt>(Operand2)) {
+            if (D->getValue() == 0) {
+                outs() << "Operand 2 -> " << D->getValue() << "\n";
+                I.replaceAllUsesWith(Operand1);
+            }
         }
-      }
-    } else if (I.getOpcode() == Instruction::Add){
-      outs() << "RILEVATA ADD " << "\n";
-      Value *Operand1 = I.getOperand(0);
-      Value *Operand2 = I.getOperand(1);
-      if (ConstantInt *C = dyn_cast<ConstantInt>(Operand1)){
-        outs() << "Opearand 1 -> " << C->getValue() << "\n";
-        if(C->getValue() == 0){
-          I.replaceAllUsesWith(Operand2);
+    }
+    // opt MUL * 1
+    else if (I.getOpcode() == Instruction::Mul) {
+        outs() << "RILEVATA MUL " << "\n";
+        Value *Operand1 = I.getOperand(0);
+        Value *Operand2 = I.getOperand(1);
+        // opt 1*x
+        if (ConstantInt *C = dyn_cast<ConstantInt>(Operand1)) {
+            outs() << "Operand 1 -> " << C->getValue() << "\n";
+            if (C->getValue() == 1) {
+                I.replaceAllUsesWith(Operand2);
+            }
         }
-      }
-      if(ConstantInt *C1 = dyn_cast<ConstantInt>(Operand2)){
-        outs() << "Opearand 2 -> " << C1->getValue() << "\n";
-          if(C1->getValue() == 0){
-            I.replaceAllUsesWith(Operand1);
+        // opt x*1
+        if (ConstantInt *C1 = dyn_cast<ConstantInt>(Operand2)) {
+            outs() << "Operand 2 -> " << C1->getValue() << "\n";
+            if (C1->getValue() == 1) {
+                I.replaceAllUsesWith(Operand1);
+            }
         }
-      }
+    //opt ADD + 1
+    } else if (I.getOpcode() == Instruction::Add) {
+        outs() << "RILEVATA ADD " << "\n";
+        Value *Operand1 = I.getOperand(0);
+        Value *Operand2 = I.getOperand(1);
+        //opt 1 + x
+        if (ConstantInt *C = dyn_cast<ConstantInt>(Operand1)) {
+            outs() << "Operand 1 -> " << C->getValue() << "\n";
+            if (C->getValue() == 0) {
+                I.replaceAllUsesWith(Operand2);
+            }
+        }
+        //opt x + 1
+        if (ConstantInt *C1 = dyn_cast<ConstantInt>(Operand2)) {
+            outs() << "Operand 2 -> " << C1->getValue() << "\n";
+            if (C1->getValue() == 0) {
+                I.replaceAllUsesWith(Operand1);
+            }
+        }
     }
     //   Manipolazione delle istruzioni
     //Instruction *NewInst = BinaryOperator::Create(
@@ -77,61 +108,56 @@ bool runOnInstruction(Instruction &I) {
 }
 
 bool runOnBasicBlock(BasicBlock &B) {
-    
     // Preleviamo le prime due istruzioni del BB
     for (auto Iter = B.begin(); Iter != B.end(); ++Iter) {
-    if (runOnInstruction(*Iter)) {
-      }
+        if (runOnInstruction(*Iter)) {
+        }
     }
     // L'indirizzo della prima istruzione deve essere uguale a quello del 
     // primo operando della seconda istruzione (per costruzione dell'esempio)
-
     
     return true;
-  }
-
-
+}
 
 // New PM implementation
 struct AlgebraicIdentity: PassInfoMixin<AlgebraicIdentity> {
-  // Main entry point, takes IR unit to run the pass on (&F) and the
-  // corresponding pass manager (to be queried if need be)
-  PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
-
-    bool Transformed = false;
-  	for (auto Iter = F.begin(); Iter != F.end(); ++Iter) {
-    if (runOnBasicBlock(*Iter)) {
-      Transformed = true;
-      }
+    // Main entry point, takes IR unit to run the pass on (&F) and the
+    // corresponding pass manager (to be queried if need be)
+    PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
+        bool Transformed = false;
+        for (auto Iter = F.begin(); Iter != F.end(); ++Iter) {
+            if (runOnBasicBlock(*Iter)) {
+                Transformed = true;
+            }
+        }
+        errs() << Transformed << "\n";
+        return PreservedAnalyses::all();
     }
-    errs() << Transformed << "\n";
-  	return PreservedAnalyses::all();
-}
 
-
-  // Without isRequired returning true, this pass will be skipped for functions
-  // decorated with the optnone LLVM attribute. Note that clang -O0 decorates
-  // all functions with optnone.
-  static bool isRequired() { return true; }
+    // Without isRequired returning true, this pass will be skipped for functions
+    // decorated with the optnone LLVM attribute. Note that clang -O0 decorates
+    // all functions with optnone.
+    static bool isRequired() { return true; }
 };
+
 } // namespace
 
 //-----------------------------------------------------------------------------
 // New PM Registration
 //-----------------------------------------------------------------------------
 llvm::PassPluginLibraryInfo getAlgebraicIdentityPluginInfo() {
-  return {LLVM_PLUGIN_API_VERSION, "AlgebraicIdentity", LLVM_VERSION_STRING,
-          [](PassBuilder &PB) {
-            PB.registerPipelineParsingCallback(
-                [](StringRef Name, FunctionPassManager &FPM,
-                   ArrayRef<PassBuilder::PipelineElement>) {
-                  if (Name == "AlgebraicIdentity") {
-                    FPM.addPass(AlgebraicIdentity());
-                    return true;
-                  }
-                  return false;
-                });
-          }};
+    return {LLVM_PLUGIN_API_VERSION, "AlgebraicIdentity", LLVM_VERSION_STRING,
+            [](PassBuilder &PB) {
+                PB.registerPipelineParsingCallback(
+                    [](StringRef Name, FunctionPassManager &FPM,
+                       ArrayRef<PassBuilder::PipelineElement>) {
+                        if (Name == "AlgebraicIdentity") {
+                            FPM.addPass(AlgebraicIdentity());
+                            return true;
+                        }
+                        return false;
+                    });
+            }};
 }
 
 // This is the core interface for pass plugins. It guarantees that 'opt' will
@@ -139,5 +165,5 @@ llvm::PassPluginLibraryInfo getAlgebraicIdentityPluginInfo() {
 // command line, i.e. via '-passes=test-pass'
 extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
 llvmGetPassPluginInfo() {
-  return getAlgebraicIdentityPluginInfo();
+    return getAlgebraicIdentityPluginInfo();
 }
